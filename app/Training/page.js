ce043c9bcbd3e1f3082e2d4eef6970b6cd23106d
Marc Norris <shaky411@googlemail.com>
2023-07-15 11:11:06 +0100

    updated training page

--- app/Training/page.js
+++ app/Training/page.js
@@ -5,10 +5,9 @@ import { InlineWidget } from "react-calendly";
 
 export default function Training() {
   return (
-    <div className="bg-slate-200 grid grid-cols-1 md:grid-cols-2 gap-5 items-center justify-center p-10">
-      <InlineWidget url="https://calendly.com/marc-norris/framepoint-support" />
-
+    <div className="bg-slate-200 grid grid-cols-1 gap-5 items-center justify-center p-10">
       <InlineWidget url="https://calendly.com/bertranddupont/selling-with-framepoint" />
+      <InlineWidget url="https://calendly.com/marc-norris/framepoint-support" />
     </div>
   );
 }
