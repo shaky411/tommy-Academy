8a2a638cef53295904a4414cb76d66dc026ee560
Marc Norris <shaky411@googlemail.com>
2023-07-25 09:30:03 +0100

    updated icons

--- components/AccordianItem.js
+++ components/AccordianItem.js
@@ -4,7 +4,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import {
   faPlusCircle,
-  faXmarkCircle
+  faXmarkCircle,
+  faCircleMinus
 } from "@fortawesome/free-solid-svg-icons";
 import { Collapse } from "react-collapse";
 
@@ -14,7 +15,7 @@ export default function AccordianItem({open, toggle, title, desc}) {
             <div className="bg-white rounded-t-lg shadow-lg text-slate-800 py-[25px] px-[50px] flex justify-between items-center cursor-pointer" onClick={toggle}>
                 <p className="text-md md:text-xl font-semibold">{title}</p>
                 <div className="text-[30px]">
-                    {open ? <FontAwesomeIcon icon={faXmarkCircle} className="bg-indigo-200 dark:bg-indigo-500 rounded-full w-6 h-6 p-3 text-indigo-500 dark:text-slate-200"/> : <FontAwesomeIcon icon={faPlusCircle} className="bg-indigo-200 dark:bg-indigo-500 rounded-full w-6 h-6 p-3 text-indigo-500 dark:text-slate-200"/>}
+                    {open ? <FontAwesomeIcon icon={faCircleMinus} className="rounded-full w-6 h-6 p-3 text-rose-400 dark:text-slate-200"/> : <FontAwesomeIcon icon={faPlusCircle} className="rounded-full w-6 h-6 p-3 text-blue-500 dark:text-slate-200"/>}
                 </div>
             </div>
 
