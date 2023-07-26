193323ed7b996e9f0e5fc64fdcb6f23482eeff35
Marc Norris <shaky411@googlemail.com>
2023-07-26 08:28:28 +0100

    updated training page

--- app/Training/page.js
+++ app/Training/page.js
@@ -6,9 +6,12 @@ import { InlineWidget } from "react-calendly";
 
 export default function Training() {
   return (
-    <div className="bg-slate-500 min-h-screen flex flex-col justify-start items-center pb-20 px-10">
+    <div className="bg-slate-500 min-h-screen flex flex-col justify-start items-center pb-20">
+      <div className="flex  flex-col justify-center items-center p-10">
       <h1 className="mt-20 mb-4 font-sans text-6xl sm:text-8xl font-bold tracking-tight text-slate-100 sm:leading-none">Training</h1>
-            <p className="mb-10 text-center">These are some frequently asked questions from our users</p>
+            <p className="mb-10 text-center">Need some training or tech support? Click below to book a session.</p>
+      </div>
+      
       <TrainingCard />
       {/* <InlineWidget url="https://calendly.com/bertranddupont/selling-with-framepoint" />
       <InlineWidget url="https://calendly.com/marc-norris/framepoint-support" /> */}
